a2c88d39d8d515dc5cbd6b89053c38cb991a2ebc
Andrey Khrolenok <andrey@khrolenok.ru>
2020-01-28 07:06:35 +0300

    Bump version

--- repositoryupdater/__init__.py
+++ repositoryupdater/__init__.py
@@ -36,7 +36,7 @@ Hass.io add-on repository approach.
 
 APP_NAME = "hassio-repo-updater"
 APP_FULL_NAME = "Community Hass.io Add-ons Repository Updater"
-APP_VERSION = "0.4.0"
+APP_VERSION = "0.4.1"
 APP_DESCRIPTION = __doc__
 
 __author__ = "Andrey Khrolenok"
